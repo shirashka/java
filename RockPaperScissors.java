e0f8676847cb9146edc409a5c79f83a1fdc92452
Shira Shkarofsky <shirashka@gmail.com>
2017-05-22 20:46:58 -0400

    Rename main.java to RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,11 +1,9 @@
 package com.shira;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class RockPaperScissors {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Shira's Rock-Paper-Scissors Game!");
